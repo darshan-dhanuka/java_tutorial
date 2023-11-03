132019613e489926028560d72151626cd3c00aa9
darshan-psl <darshan.dhanuka@pokersportsleague.com>
2023-11-04 01:56:51 +0530

    continued commit

--- Main.java
+++ Main.java
@@ -2,7 +2,12 @@
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        byte age = 30;
+        long viewCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        System.out.println(viewCount);
     }
 }
 
